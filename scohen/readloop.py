d2934b64cd2a696b2720e3d92d7956aed19e3a26
marc w <marcwe@pcom.edu>
2014-12-18 10:48:28 -0500

    file list

---
+++ scohen/readloop.py
@@ -0,0 +1,6 @@
+urlFile = open("mailList.txt", "r+")
+mailList = [i.strip() for i in urlFile.readlines()]
+for entries in i:
+  print "%r" % (entries)
+for entries in mailList:
+  print '%r' % (entries)  
